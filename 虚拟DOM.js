4f4e25af1597a508e3b86d1069e176f5951eb697
wangmiaozero <1061214467@qq.com>
2019-03-24 23:37:50 +0800

    react-js学习

---
+++ 虚拟DOM.js
@@ -0,0 +1,19 @@
+//模拟dom树
+var div = {
+    tagName:'div',
+    attrs:{
+        id:'mydiv',
+        title:"说实话",
+        'data-index':'0'
+    },
+    childrens:[
+        '我好帅',
+        {
+            tagName:'p',
+            attrs:{},
+            childrens:[
+                '哈哈哈哈'
+            ]
+        }
+    ]
+}
