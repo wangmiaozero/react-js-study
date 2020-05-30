e4527bd9d87f16cacf8774901d3accf6f8a2aa31
wangmiaozero <1061214467@qq.com>
2020-05-30 21:04:46 +0800

    1

--- hooks-demo/src/Example4.js
+++ hooks-demo/src/Example4.js
@@ -1,10 +1,10 @@
 /*
- * @Description: 父子组件传值
+ * @Description: useReducer使用
  * @Version: 1.0
  * @Autor: wangmiao
  * @Date: 2020-05-28 13:42:06
  * @LastEditors: wangmiao
- * @LastEditTime: 2020-05-30 16:27:48
+ * @LastEditTime: 2020-05-30 17:54:57
  */ 
 
 /* 
@@ -13,16 +13,9 @@
 */
 
 // hooks 写法
-import React, { useState,useEffect,createContext, useContext  } from 'react';
-// 相当于创建了一个组件
-const CountCountext = createContext();
-// 子组件
-function Counter(){
-  let count = useContext(CountCountext)
-  return(<h2>这是父组件的值: {count}</h2>)
-}
+import React, { useState,useEffect,createContext, useContext,useReducer } from 'react'
 
-function Example3(){
+function Example4(){
   // useState 申明 
    const [ count, setCount ] = useState(0)// 数组结构
   // useEffect 相当于componentDidMount/componentDidUpdate
@@ -35,11 +28,11 @@ function Example3(){
     <div>
        <p> 数值:{count}</p>
        <button onClick={()=>setCount(count+1)}>增加</button>
-       <CountCountext.Provider value = {count}>
+     {/*   <CountCountext.Provider value = {count}>
          <Counter/>
-       </CountCountext.Provider>
+       </CountCountext.Provider> */}
     </div>
   )
 }
 
-export default Example3;
+export default Example4;
